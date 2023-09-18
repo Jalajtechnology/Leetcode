4f0c1ed145061a2faa4ad834c465dda8e2b1e6b3
Jalajtechnology <141317612+Jalajtechnology@users.noreply.github.com>
2023-09-18 16:12:08 +0530

    Time: 4 ms (87.63%), Space: 18.9 MB (13.66%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,10 +4,12 @@ public:
         int i=0;
         int j=0;
         int count=0;
+        vector<int>result;
         while(j<nums.size())
         {
             if(i==j)
             {
+                result.push_back(nums[i]);
                 count=count+1;
                 j++;
             }
@@ -17,8 +19,20 @@ public:
             }
             else if(i!=j && nums[i]==nums[j])
             {
-                nums.erase(nums.begin()+j);
+                j++;
             }
+            
+        }
+        int k;
+        for( k=0;k<result.size();k++)
+        {
+            nums[k]=result[k];
+        }
+        if(k<nums.size() && nums.size()!=1 )
+        {
+        auto first=nums.begin()+k;
+        auto second=nums.begin()+nums.size();
+        nums.erase(first,second);
         }
         return count;
 
