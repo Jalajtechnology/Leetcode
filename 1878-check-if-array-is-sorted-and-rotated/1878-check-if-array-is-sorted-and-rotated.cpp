2cc51ac7a77551cc6e6c4b4b7b59ebeb07f1d272
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2023-09-25 13:02:43 +0530

    Time: 2 ms (43.53%), Space: 9 MB (8.39%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,25 +1,79 @@
 class Solution {
 public:
-    bool check(vector<int>& nums) {
-        int count=0;
-        for(int i=0;i<nums.size()-1;i++)
+    int pivotelement(vector<int>nums,int n)
+    {
+        int start=0;
+        int end=n-1;
+        int mid=(start+end)/2;
+        while(start<=end)
         {
-            if(nums[i]>nums[i+1])
+            if(start==end)
             {
-                count++;
-
+                return start;
             }
+            else if(mid>start&&nums[mid]<nums[mid-1])
+            {
+                return mid-1;
+            }
+            else if(mid<end && nums[mid]>nums[mid+1])
+            {
+                return mid;
+            }
+            else if(nums[mid]>=nums[start])
+            {
+                start=mid+1;
+            }
+            else if(nums[mid]<nums[start])
+            {
+                end=mid-1;
+            }
+            mid=(start+end)/2;
         }
-        if(nums[nums.size()-1]>nums[0])
-        {
-            count++;
-        }
-        if(count<=1)
-        {
-            return true;
-        }
-        return false;
+        return -1;
+    }
+    
+    bool check(vector<int>& nums) 
+    {
+       int pivotindex= pivotelement(nums,nums.size());
+       int flag1=1;
+       int flag2=1;
+       int flag3=1;
+    
+         
+         for(int i=0;i<pivotindex-1;i++)
+         {
+             if(nums[i]>nums[i+1])
+             {
+                 flag1=0;
+                 break;
+             }
+         }
+         for(int i=pivotindex+1;i<nums.size()-1;i++)
+         {
+             if(nums[i]>nums[i+1])
+             {
+                 flag2=0;
+                 break;
+             }
+         }
+         if(pivotindex==nums.size()-1)
+         {
+             return flag1 && flag2;
+         }
+         else if(pivotindex!=nums.size()-1)
+         {
+             if(nums[nums.size()-1]>nums[0])
+             {
+                 flag3=0;
+             }
+         }
+         return flag1&&flag2&&flag3;
+         
+    }
     
         
-    }
+
+    
+       
+    
 };
