6330fb11b2b536f5ad4f71a55a36a06daefaa937
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2023-10-01 15:48:06 +0530

    Time: 146 ms (17.57%), Space: 84.3 MB (16.47%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int>hashtable;
+        for(int i=0;i<nums.size();i++)
+        {
+            hashtable[nums[i]]++;
+            if(hashtable[nums[i]]>1)
+            {
+                return nums[i];
+            }
+        }
+        return -1;
+        
+        
+    }
+};
