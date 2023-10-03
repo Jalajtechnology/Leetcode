af856fc974dc1073db45b86e225edd7c5e224569
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2023-10-03 11:39:29 +0530

    Time: 3 ms (64.92%), Space: 11.6 MB (5.51%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    int findelement(int start,int end,vector<int>nums,int target)
+    {
+        int mid=(start+end)/2;
+        while(start<=end)
+        {
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]<target)
+            {
+                start=mid+1;
+            }
+            else if(nums[mid]>target)
+            {
+                end=mid-1;
+            }
+            mid=(start+end)/2;
+        }
+        return -1;
+    }
+    int findpivotindex(vector<int>&nums)
+    {
+        int start=0;
+        int end=nums.size()-1;
+        int mid=(start+end)/2;
+        while(start<=end)
+        {
+            if(start==end)
+            {
+                return start;
+            }
+            else if(mid>start && nums[mid]<nums[mid-1])
+            {
+                return mid-1;
+            }
+            else if(mid<end && nums[mid]>nums[mid+1])
+            {
+                return mid;
+            }
+            else if(nums[mid]>=nums[start])
+            {
+                start=mid+1;
+            }
+            else if(nums[mid]<nums[start])
+            {
+                end=mid-1;
+            }
+            mid=(start+end)/2;
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        int pivotindex=findpivotindex(nums);
+        int l=0;
+        if(pivotindex+1==nums.size())
+        {
+             l=findelement(0,pivotindex,nums,target);
+            
+        }
+        else if(pivotindex+1<nums.size())
+        {
+            l=findelement(0,pivotindex,nums,target);
+            cout<<l;
+            if(l==-1)
+            {
+                int q=findelement(pivotindex+1,nums.size()-1,nums,target);
+                return q;
+            }
+        }
+        return l;
+
+        
+    }
+};
