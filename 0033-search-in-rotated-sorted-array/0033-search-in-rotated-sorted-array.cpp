c534fd9587e64203e1fb0e4462ca88b9b9d8de27
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2023-10-05 16:53:28 +0530

    Time: 0 ms (100%), Space: 11.5 MB (6.11%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    int findelement(int start,int end,vector<int>nums,int target)
-    {
+    int search(vector<int>& nums, int target) {
+        int start=0;
+        int end=nums.size()-1;
         int mid=(start+end)/2;
         while(start<=end)
         {
@@ -9,74 +10,32 @@ public:
             {
                 return mid;
             }
-            else if(nums[mid]<target)
+            if(nums[mid]>=nums[start])//identification of sorted part
             {
-                start=mid+1;
+                if(target>=nums[start] && target <nums[mid])//iftarget coming in sorted part eliminating other part
+                {
+                    end=mid-1;
+                }
+                else //if target not coming in sorted part eliminating sorted part
+                {
+                    start=mid+1;
+                }
             }
-            else if(nums[mid]>target)
+            else
             {
-                end=mid-1;
-            }
-            mid=(start+end)/2;
-        }
-        return -1;
-    }
-    int findpivotindex(vector<int>&nums)
-    {
-        int start=0;
-        int end=nums.size()-1;
-        int mid=(start+end)/2;
-        while(start<=end)
-        {
-            
-             if(mid>start && nums[mid]<nums[mid-1])
-            {
-                return mid-1;
-            }
-            else if(mid<end && nums[mid]>nums[mid+1])
-            {
-                return mid;
-            }
-            else if(nums[mid]>=nums[start])
-            {
-                start=mid+1;
-            }
-            else if(nums[mid]<nums[start])
-            {
-                end=mid-1;
+                if(target>nums[mid] && target<=nums[end])
+                {
+                    start=mid+1;
+                }
+                else 
+                {
+                    end=mid-1;
+                }
             }
             mid=(start+end)/2;
-        }
-        return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        if(nums.size()==1)
-        {
-            if(target==nums[0])
-            {
-                return 0;
-            }
-            return -1;
-        }
-        int pivotindex=findpivotindex(nums);
-        int l=0;
-        if(pivotindex+1==nums.size())
-        {
-             l=findelement(0,pivotindex,nums,target);
-            
-        }
-        else if(pivotindex+1<nums.size())
-        {
-            l=findelement(0,pivotindex,nums,target);
-            cout<<l;
-            if(l==-1)
-            {
-                int q=findelement(pivotindex+1,nums.size()-1,nums,target);
-                return q;
-            }
-        }
-        return l;
 
+        }
+        return -1;
         
     }
 };
