85882b89cd28ed776e733e81f8563245ddee5065
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2023-10-12 18:22:33 +0530

    Time: 9 ms (57.26%), Space: 7.9 MB (10.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t)
+        set<char>hashtable;
+        int i=0;
+        int j=0;
+        while(i<s.length())
         {
-            return true;
+            hashtable.insert(s[i]);
+            i++;
         }
-        return false;
+        while(j<t.length())
+        {
+            hashtable.insert(t[j]);
+            j++;
+        }
+        unordered_map<char,int>hashtable1;
+        unordered_map<char,int>hashtable2;
+        for(int i=0;i<s.length();i++)
+        {
+            hashtable1[s[i]]++;
+        }
+        for(int j=0;j<t.size();j++)
+        {
+            hashtable2[t[j]]++;
+        }
+        for(auto it:hashtable)
+        {
+            if(hashtable1[it] != hashtable2[it])
+            {
+                return false;
+            }
+        }
+        return true;
         
     }
 };
