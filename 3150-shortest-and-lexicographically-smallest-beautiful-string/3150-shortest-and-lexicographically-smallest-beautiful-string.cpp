42cc6a33a42d93cba8b229dc9cb0a28aac532543
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2023-10-15 17:26:25 +0530

    Time: 8 ms (89.47%), Space: 7.5 MB (89.47%) - LeetHub

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,61 +1,65 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
+        string str="";
         vector<string>arr;
         int min=INT_MAX;
-        if(k==1)
+        int i=0;
+        int j=0;
+        int count=0;
+        while(i<s.length())
         {
-            for(int i=0;i<s.length();i++)
-            {
-                if(s[i]=='1')
-                {
-                    return "1";
-                }
-
-            }
-            return "";
-        }
-        for(int i=0;i<s.length();i++)
-        {
-            int count=0;
-            string str="";
-            str+=s[i];
-            if(s[i]=='1')
-            {
-                count++;
-            }
-            for(int j=i+1;j<s.length();j++)
+            str=s.substr(i,j-i);
+            while(count!=k && j<s.length())
             {
                 str+=s[j];
                 if(s[j]=='1')
                 {
-                    count++;
+                count++;
                 }
-                if(count==k)
+                j++;
+            }
+            cout<<str<<endl;
+            if( count<k)
+            {
+                break;
+            }
+            else if(count==k)
+            {
+                arr.push_back(str);
+                int length=j-i;
+                if(length<min)
                 {
-                    int length=j-i+1;
-                    if(length<min)
-                    {
-                        min=length;
-                    }
-                    arr.push_back(str);
+                    min=length;
                 }
+                
+                
+               
+                
+                
+                if(s[i]=='1')
+                {
+                    count=count-1;
+                }
+                i++;
+
+
             }
         }
         if(min==INT_MAX)
         {
             return "";
-
         }
         sort(arr.begin(),arr.end());
         for(int i=0;i<arr.size();i++)
         {
-            if(min==(arr[i].length()))
+            if(min==arr[i].length())
             {
                 return arr[i];
             }
         }
         return arr[0];
+         
        
         
 
