4d9524acc8dfde514691cf05ee7f0a5193644aec
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2023-11-15 07:29:06 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (66.34%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        unordered_map<char,int>hashtable;
+        //unordered_map<char,int>hashtable;
+       // for(int i=0;i<word.size();i++)
+       // {
+            //if(hashtable.find(word[i])==hashtable.end())
+            //{
+           // hashtable[word[i]]=i;
+           // }
+       // }
+        //if(hashtable.find(ch) !=hashtable.end())
+        //{
+           // int k=hashtable[ch];
+           // reverse(word.begin(),word.begin()+k+1);
+        //}//
+        int index=-1;
         for(int i=0;i<word.size();i++)
         {
-            if(hashtable.find(word[i])==hashtable.end())
+            if(word[i]==ch)
             {
-            hashtable[word[i]]=i;
+                index=i+1;
+                break;
             }
         }
-        if(hashtable.find(ch) !=hashtable.end())
+        if(index !=-1)
         {
-            int k=hashtable[ch];
-            reverse(word.begin(),word.begin()+k+1);
+            reverse(word.begin(),word.begin()+index);
         }
         return word;
         
