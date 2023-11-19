9667c91e9f5b7532f6f2f5dcd3a4bc626cf7153c
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2023-11-19 21:37:10 +0530

    Time: 10 ms (11.99%), Space: 11.8 MB (12.50%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.cpp
+++ 0969-pancake-sorting/0969-pancake-sorting.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     vector<int> pancakeSort(vector<int>& arr) {
-        if(arr.size()==1)
-        {
-            return {};
-        }
-        
-        vector<int>ans;
+        unordered_map<int,int>hashtable;
+        vector<int>brr;
         for(int i=arr.size();i>0;i--)
         {
-            unordered_map<int,int>hashtable;
             for(int i=0;i<arr.size();i++)
             {
                 hashtable[arr[i]]=i;
             }
             if(hashtable.find(i)!=hashtable.end())
             {
-                if(hashtable[i]!=i-1)
-                {
-                    ans.push_back(hashtable[i]+1);
-                    ans.push_back(i);
-                    reverse(arr.begin(),arr.begin()+hashtable[i]+1);
-                    reverse(arr.begin(),arr.begin()+i);
-                }
+            if(hashtable[i]!=i-1)
+            {
+                reverse(arr.begin(),arr.begin()+hashtable[i]+1);
+                brr.push_back(hashtable[i]+1);
+                reverse(arr.begin(),arr.begin()+i);
+                brr.push_back(i);
                 
             }
+            }
+            
         }
-        return ans;
-        
+        return brr;
+         
+       
     }
 };
