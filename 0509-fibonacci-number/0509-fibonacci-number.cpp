dde6c34c827fd8975905b7311d75909d9dd00821
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2024-01-07 18:50:13 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (25.62%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int topdownapproach(vector<int>&dp,int n)
+   /* int topdownapproach(vector<int>&dp,int n)
     {
         if(n==0 ||n==1)
         {
@@ -12,10 +12,31 @@ public:
         }
         dp[n]=topdownapproach(dp,n-1)+topdownapproach(dp,n-2);
         return dp[n];
+    }*/
+    int bottomupapproach(int n)
+    {
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        if(n==0)
+        {
+            return dp[n];
+        }
+        dp[1]=1;
+        if(n==1)
+        {
+            return dp[n];
+        }
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
     int fib(int n) { 
-        vector<int>dp(n+1,-1);
+       /* vector<int>dp(n+1,-1);
         int ans =topdownapproach(dp,n);
+        return ans;*/
+        int ans=bottomupapproach( n);
         return ans;
         
     }
