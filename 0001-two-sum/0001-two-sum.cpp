51a8ac18deb9592be24daa9108b21bf668a08601
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2024-03-19 15:56:33 +0530

    Time: 8 ms (79.63%), Space: 14 MB (36.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int>hashtable;
+        vector<int>arr;
         for(int i=0;i<nums.size();i++)
         {
-            if(hashtable.find((target-nums[i]))!=hashtable.end())
-            {
-                return {hashtable[(target-nums[i])],i};
-            }
-            else
+            if((hashtable.find(target-nums[i])==hashtable.end()))
             {
                 hashtable[nums[i]]=i;
             }
+            else
+            {
+               arr.push_back(hashtable[target-nums[i]]);
+               arr.push_back(i);
+                break;
+            }
+           
+            
         }
-        return {0,0};
-        
-      
-     
-        
+        return arr;
     }
 };
