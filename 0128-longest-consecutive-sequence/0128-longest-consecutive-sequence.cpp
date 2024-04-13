d5bf7149664f4d51fefb3f0f6b11fca7976af0b4
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2024-04-13 21:18:48 +0530

    Time: 851 ms (14.50%), Space: 74.1 MB (47.82%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,41 +1,7 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-       /*** sort(nums.begin(),nums.end());
-        unordered_map<int,int>hashtable;
-        if(nums.size()==0)
-        {
-            return 0;
-        }
-        
-        int max=INT_MIN;
-        for(int i=0;i<nums.size();i++)
-        {
-            hashtable[nums[i]]=0;
-        }
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-            
-            if(hashtable[nums[i]]==0)
-            {
-                int  count=1;
-                int j=nums[i]-1;
-                while(hashtable.find(j)!=hashtable.end())
-                {
-                    count++;
-                    hashtable[j]=1;
-                    j=j-1;
-                }
-                if(max<count)
-                {
-                    max=count;
-                }
-            }
-           
-        }
-        return max; Time complexity is nlogn***/
-        
-        unordered_map<int,int>hashtable;
+        /*unordered_map<int,int>hashtable;
         for(int i=0;i<nums.size();i++)
         {
             hashtable[nums[i]]=0;
@@ -43,25 +9,51 @@ public:
         int max=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(hashtable[nums[i]]==0 && hashtable.find(nums[i]-1)==hashtable.end())
+            if(hashtable[nums[i]]!=1)
             {
                 hashtable[nums[i]]=1;
                 int count=1;
-                int k=nums[i]+1;
-                while(hashtable.find(k)!=hashtable.end())
+                int j=nums[i]-1;
+                while(hashtable.find(j)!=hashtable.end())
                 {
+                    hashtable[j]=1;
                     count++;
-                    hashtable[k]=1;
-                    k=k+1;
+                    j--;
                 }
-                if(max<count)
+                if(count>max)
                 {
                     max=count;
                 }
             }
         }
+        return max;*/
+        unordered_map<int,int>hashtable;
+        int max=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            hashtable[nums[i]]=0;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(hashtable.find(nums[i]-1)==hashtable.end())
+            {
+                int count=1;
+                int j=nums[i]+1;
+                while(hashtable.find(j)!=hashtable.end())
+                {
+                    count++;
+                    j++;
+                }
+                if(max<count)
+                {
+                    max=count;
+                }
+                    
+                
+            }
+        }
         return max;
-        
+      
         
         
         
