8f97e69586787490c71be82491468686ceb94c05
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2024-04-26 21:33:13 +0530

    Time: 266 ms (67.97%), Space: 171.5 MB (83.44%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -2,53 +2,64 @@ class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
         unordered_map<string,int>hashtable;
-        bool flag=0;
-        int length=0;
+        int flag=0;
         for(int i=0;i<words.size();i++)
         {
             hashtable[words[i]]++;
         }
+        int count=0;
         
-      for(int i=0;i<words.size();i++)
-      {
-          if(hashtable[words[i]]!=0 && words[i][0] !=words[i][1] )
-          {
-              string k=words[i];
-              reverse(k.begin(),k.end());
-              if(hashtable.find(k)!=hashtable.end())
-              {
-                  length=length+4*(min(hashtable[words[i]],hashtable[k]));
-                  hashtable[k]=0;
-              }
-          }
-          else if(hashtable[words[i]]!=0 && words[i][0]==words[i][1])
-          {
-              if(hashtable[words[i]]%2==0 )
-              {
-                  length=length+2*hashtable[words[i]];
-                  
-              }
-              else if(  hashtable[words[i]]%2 !=0)
-              {
-                  if(flag==0)
-                  {
-                      length=length+2*(hashtable[words[i]]);
-                      flag=1;
-                          
-                  }
-                  else if(flag==1)
-                  {
-                      length=length+2*(hashtable[words[i]]-1);
-                      
-                  }
-                  
-              }
-          }
-          hashtable[words[i]]=0;
-      }
-        return length;
-                              
-                              
+        int max2=0;
+        for(int i=0 ;i<words.size();i++)
+        {
+            if(hashtable[words[i]]!=0)
+            {
+                if(words[i][0]!=words[i][1])
+                {
+                    string k=words[i];
+                    reverse(k.begin(),k.end());
+                    cout<<k;
+                    if(hashtable.find(k)!=hashtable.end())
+                    {
+                        count=count+(4*(min(hashtable[words[i]],hashtable[k])));
+                        cout<<count;
+                        
+                        hashtable[k]=0;
+                    }
+                    
+                }
+                else if(words[i][0]==words[i][1])
+                {
+                    if((hashtable[words[i]]%2)==0)
+                    {
+                        count=count+(2*(hashtable[words[i]]));
+                        
+                    }
+                    else if((hashtable[words[i]]%2) !=0)
+                    {
+                       
+                        if(flag==0)
+                        {
+                            count=count+(2*(hashtable[words[i]]));
+                            flag=1;
+                        }
+                        else if(flag !=0)
+                        {
+                            count=count+(2*(hashtable[words[i]]-1));
+                        }
+                        
+                        
+                    }
+                    
+                    
+                
+                }
+                
+                hashtable[words[i]]=0;
+            }
+        }
+        
+        return count;
                               
         
                               
