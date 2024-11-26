d68ec6d90f5012596acd5510360f03f274b2fa21
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2024-11-26 16:57:53 +0530

    Time: 35 ms (94.45%), Space: 45.4 MB (63.25%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>hashtable;
         int sum=0;
         int count=0;
-        unordered_map<int,int>hashtable;
         for(int i=0;i<nums.size();i++)
         {
             sum=sum+nums[i];
             if(sum==k)
             {
-                count=count+1;
+                count++;
             }
-            if(hashtable.find(sum-k)!=hashtable.end())
+            if(hashtable.find(sum-k) !=hashtable.end())
             {
                 count=count+hashtable[sum-k];
             }
             hashtable[sum]++;
+
         }
         return count;
         
